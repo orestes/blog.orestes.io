f3790f2b614681005bc783ff21b6c6473efa5fbd
orestes <orestes.ca@gmail.com>
2013-06-27 12:29:59 +0200

    Remove JQuery dependency and script.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var list = document.querySelectorAll('div.content>img');
-    for(var i = 0; list.length; i++) {
-            list[i].onclick = function() { window.open(this.src) };
-    }
-})();
